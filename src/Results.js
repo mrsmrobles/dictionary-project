e1b266a18d2ff0e74fa2e5783d5f35b1b749ab0a
mrsmrobles <106035397+mrsmrobles@users.noreply.github.com>
2025-08-22 20:50:06 -0700

    added a limit to the results array loop

--- src/Results.js
+++ src/Results.js
@@ -8,11 +8,15 @@ export default function Results(props) {
         <h1>{props.results.word}</h1>
         <h3>{props.results.phonetic}</h3>
         {props.results.meanings.map(function (meaning, index) {
+          if (index < 4) {
             return (
-                <div key={index}>
-                    <Meaning meaning={meaning} />
-                </div>
+              <div key={index}>
+                <Meaning meaning={meaning} />
+              </div>
             );
+          } else {
+            return null;
+          }
         })}
       </div>
     );
