3ab41d2d48a21d072d18af2e96160131ec6e9a43
mrsmrobles <106035397+mrsmrobles@users.noreply.github.com>
2025-08-22 20:44:08 -0700

    set up a component to display the results and loop through an array of multiple meanings.

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,21 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
+import Results from "./Results";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
     console.log(response.data);
+    setResults(response.data);
   }
 
   function search(event) {
     event.preventDefault();
 
+    //api documentation: https://www.shecodes.io/learn/apis/dictionary
     let apiKey = "0afcd2ao9bb24495ta6dd7a01113764b";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
 
@@ -32,6 +36,7 @@ export default function Dictionary() {
           onChange={handleKeywordChange}
         />
       </form>
+      <Results results={results}/>
     </div>
   );
 }
---
+++ src/Meaning.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Meaning(props) {
+  return (
+    <div className="Meaning">
+      <h4>{props.meaning.partOfSpeech}</h4>
+      <p>
+        {props.meaning.definition}
+        <br />
+        {props.meaning.example}
+      </p>
+    </div>
+  );
+}
