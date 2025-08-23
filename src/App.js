ea7847f202eec6fbd6fab1f7322564cddde92e18
mrsmrobles <106035397+mrsmrobles@users.noreply.github.com>
2025-08-22 19:49:21 -0700

    built search and added dictionary api integration

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
       <img
         src="https://www.shecodes.io/assets/branding/logo-shecodes-3dfa60aeab8ef361842da5a2b6d46db3af1b7afafefee3dde0a9846389de754b.png"
         alt="logo"
         className="img-fluid App-logo"
-      />
-      Hello
+      /></header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">Coded by Madison Robles</footer>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
+  function search(event) {
+    event.preventDefault();
+
+    let apiKey = "0afcd2ao9bb24495ta6dd7a01113764b";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input
+          type="search"
+          autoFocus={true}
+          placeholder="Search for a word.."
+          onChange={handleKeywordChange}
+        />
+      </form>
+    </div>
+  );
+}
