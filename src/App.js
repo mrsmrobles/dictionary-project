062a5b907b1a818f87a8c9e12e84aaf7915610f7
mrsmrobles <106035397+mrsmrobles@users.noreply.github.com>
2025-08-22 18:50:01 -0700

    added header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <img
+        src="https://www.shecodes.io/assets/branding/logo-shecodes-3dfa60aeab8ef361842da5a2b6d46db3af1b7afafefee3dde0a9846389de754b.png"
+        alt="logo"
+        className="img-fluid App-logo"
+      />
+      Hello
+    </div>
+  );
 }
 
 export default App;
