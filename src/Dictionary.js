7322b42c1f84d7296d8f4247ce984c2111c0ada6
mrsmrobles <106035397+mrsmrobles@users.noreply.github.com>
2025-08-24 13:08:11 -0700

    Added styling to sections and created default search

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,17 +4,17 @@ import "./Dictionary.css";
 import Results from "./Results";
 
 export default function Dictionary() {
-  let [keyword, setKeyword] = useState("");
+  let [keyword, setKeyword] = useState("sunset");
   let [results, setResults] = useState(null);
+  let [loaded, setLoaded] = useState(false);
+
 
   function handleResponse(response) {
     console.log(response.data);
     setResults(response.data);
   }
 
-  function search(event) {
-    event.preventDefault();
-
+  function search() {
     //api documentation: https://www.shecodes.io/learn/apis/dictionary
     let apiKey = "0afcd2ao9bb24495ta6dd7a01113764b";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
@@ -22,21 +22,41 @@ export default function Dictionary() {
     axios.get(apiUrl).then(handleResponse);
   }
 
+  
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
+  
+  function load() {
+    setLoaded(true);
+    search();
+  }
 
+if (loaded) {
   return (
     <div className="Dictionary">
-      <form onSubmit={search}>
-        <input
-          type="search"
-          autoFocus={true}
-          placeholder="Search for a word.."
-          onChange={handleKeywordChange}
-        />
-      </form>
-      <Results results={results}/>
+      <section>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="search"
+            autoFocus={true}
+            placeholder="Search for a word.."
+            onChange={handleKeywordChange}
+          />
+        </form>
+        <div className="hint">ex: sunset, book, swimming...</div>
+      </section>
+      <Results results={results} />
     </div>
   );
+} else {
+ load();
+ return "Loading..";
+}
 }
--- src/Results.js
+++ src/Results.js
@@ -1,18 +1,20 @@
 import React from "react";
 import Meaning from "./Meaning";
+import "./Results.css";
 
 export default function Results(props) {
   if (props.results) {
     return (
       <div className="Results">
-        <h1>{props.results.word}</h1>
-        <h3>{props.results.phonetic}</h3>
+        <section>
+        <h2>{props.results.word}</h2>
+        <h3>{props.results.phonetic}</h3></section>
         {props.results.meanings.map(function (meaning, index) {
           if (index < 4) {
             return (
-              <div key={index}>
+              <section key={index}>
                 <Meaning meaning={meaning} />
-              </div>
+              </section>
             );
           } else {
             return null;
